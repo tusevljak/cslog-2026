f859e79617f5e4351d521d1f28d912d462c0ab0e
tusevljak <ivan.tusevljak@gmail.com>
2026-07-07 02:17:11 +0200

    Header: CTA 'Besplatan upit' → 'Free Quote' on /en routes

--- components/Header.tsx
+++ components/Header.tsx
@@ -236,7 +236,7 @@ export default function Header() {
             <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
               <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
             </svg>
-            Besplatan upit
+            {isEn ? 'Free Quote' : 'Besplatan upit'}
           </a>
         </div>
       </div>
@@ -334,7 +334,7 @@ export default function Header() {
               <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
                 <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
               </svg>
-              Besplatan upit
+              {isEn ? 'Free Quote' : 'Besplatan upit'}
             </a>
           </div>
 
